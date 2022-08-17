73521d3c75938b409e7c111e9135606615a9ba8f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-16 23:02:46 -0300

    Add files via upload

--- registro_tipos_veiculos.R
+++ registro_tipos_veiculos.R
@@ -13,15 +13,46 @@
 # Pacotes necessários para as análises -----------------------------------------------------------------------------------------------------
 
 library(tidyverse)
+library("scales")
 
 # Carregar dados ---------------------------------------------------------------------------------------------------------------------------
 
 veh <- read.csv("new-vehicles-type-area.csv")
 view(veh)
+names(veh)
 
 # Selecionar dados -------------------------------------------------------------------------------------------------------------------------
 
 veh1 <- veh %>%
   select(-Code) %>%
   view()
+
+# Gráficos ---------------------------------------------------------------------------------------------------------------------------------
+
+options(scipen = 999)
+
+g1 <- ggplot(veh1, aes(x = Year, y = battery_electric_number)) +
+  geom_col() +
+  scale_y_continuous(labels = comma)
+g1
+
+g2 <- ggplot(veh1, aes(x = Year, y = plugin_hybrid_number)) +
+  geom_col() +
+  scale_y_continuous(labels = comma)
+g2
+
+g3 <- ggplot(veh1, aes(x = Year, y = full_mild_hybrid_number)) +
+  geom_col() +
+  scale_y_continuous(labels = comma)
+g3
+
+g4 <- ggplot(veh1, aes(x = Year, y = petrol_number)) +
+  geom_col() +
+  scale_y_continuous(labels = comma)
+g4
+
+g5 <- ggplot(veh1, aes(x = Year, y = diesel_gas_number)) +
+  geom_col() +
+  scale_y_continuous(labels = comma)
+g5
 
