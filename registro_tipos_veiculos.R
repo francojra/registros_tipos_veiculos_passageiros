f9b41519c7b9af66f4aecc04b8167ed1d44733ef
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-16 21:42:53 -0300

    Add files via upload

---
+++ registro_tipos_veiculos.R
@@ -0,0 +1,19 @@
+
+# Registros de veículos por tipo ----------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 16/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/transport -----------------------------------------------------------------------------------------
+
+# Sobre o conjunto de dados ----------------------------------------------------------------------------------------------------------------
+
+### Os dados mostram os registros dos tipos de veículos de passageiros.
+### Os dados estão discriminados por: petróleo, gás óleo, híbridos completos,
+### híbridos elétricos plug-in e veículos com bateria totalmente elétrica.
+
+# Pacotes necessários para as análises -----------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+
+
+
+
